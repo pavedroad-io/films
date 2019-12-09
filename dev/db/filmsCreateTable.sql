2736c09b3c995fe1fe253006e7b122c0e29e7135
John Scharber <john@pavedroad.io>
2019-12-09 13:57:07 -0800

    Initial commit for films repo

--- dev/db/filmsCreateTable.sql
+++ dev/db/filmsCreateTable.sql
@@ -1,7 +1,7 @@
 
-CREATE TABLE IF NOT EXISTS acme.films (
+CREATE TABLE IF NOT EXISTS AcmeDemo.films (
     FilmsUUID UUID DEFAULT uuid_v4()::UUID PRIMARY KEY,
     films JSONB
 );
 
-CREATE INDEX IF NOT EXISTS filmsIdx ON acme.films USING GIN (films);
+CREATE INDEX IF NOT EXISTS filmsIdx ON AcmeDemo.films USING GIN (films);
